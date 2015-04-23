8ea902bf95ec2696b44e65a1a31713b537d782f3
Daniel Diaz <diaz.carrete@facebook.com>
2015-04-23 23:46:38 +0200

    Some progress with the diagrams.

--- library/VDPT/Diagrams.hs
+++ library/VDPT/Diagrams.hs
@@ -19,19 +19,25 @@ import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 import qualified Data.ByteString.Lazy as BL
 import Control.Applicative
+--import Control.Lens ((.~))
 
 import Diagrams.Prelude
 import Diagrams.TwoD.Size
 import Diagrams.TwoD.Arrow
 import Diagrams.TwoD.Layout.Tree
 import Diagrams.Backend.SVG
-import Lucid
+import Lucid (renderBS)
 
 import VDPT.Types
 
 renderNodeTreeDia :: forall a. Tree a -> BL.ByteString
-renderNodeTreeDia = renderBS . renderDia SVG (SVGOptions (dims2D (1000::Float) 700) [] "") . renderNodeTree
+renderNodeTreeDia = renderBS . renderDia SVG (SVGOptions (mkHeight (750::Double)) [] "") . renderNodeTree
+--renderNodeTreeDia = renderBS . renderDia SVG (SVGOptions (dims2D (1000::Float) 700) [] "") . renderNodeTree
 --renderNodeTreeDia = renderBS . renderDia SVG (SVGOptions (mkWidth (500::Float)) [] "") . renderNodeTree
 
-renderNodeTree :: forall a b. Renderable (Path V2 Float) b => Tree a -> QDiagram b V2 Float Any
-renderNodeTree nodeTree = renderTree (const (circle 1)) arrowBetween (symmLayout nodeTree)
+renderNodeTree :: Tree a -> QDiagram SVG V2 Double Any
+--renderNodeTree nodeTree = renderTree (\_ -> circle 1 # fc white `atop` text "FOO") arrowBetween (symmLayout nodeTree)
+renderNodeTree nodeTree = renderTree 
+    (\_ -> text "FOO" # font "monospace" `atop` square 1 # fc white) 
+    (~~) 
+    (symmLayout' (with{ _slHSep = 3,  _slVSep = 3}) nodeTree)
