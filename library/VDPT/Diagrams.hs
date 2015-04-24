59987eb9d7cb4aa168ebb380fdacea226ff54365
Daniel Diaz <diaz.carrete@facebook.com>
2015-04-25 01:04:01 +0200

    Progress with trace diagrams.

--- library/VDPT/Diagrams.hs
+++ library/VDPT/Diagrams.hs
@@ -30,14 +30,22 @@ import Lucid (renderBS)
 
 import VDPT.Types
 
-renderNodeTreeDia :: forall a. Tree a -> BL.ByteString
+abbreviate :: Attributes -> T.Text 
+abbreviate = mconcat . map (fst . T.splitAt 1) . T.words . _nodeType
+
+renderNodeTreeDia :: Tree Attributes -> BL.ByteString
 renderNodeTreeDia = renderBS . renderDia SVG (SVGOptions (mkHeight (750::Double)) [] "") . renderNodeTree
 --renderNodeTreeDia = renderBS . renderDia SVG (SVGOptions (dims2D (1000::Float) 700) [] "") . renderNodeTree
 --renderNodeTreeDia = renderBS . renderDia SVG (SVGOptions (mkWidth (500::Float)) [] "") . renderNodeTree
 
-renderNodeTree :: Tree a -> QDiagram SVG V2 Double Any
+
+renderNodeTree :: Tree Attributes -> QDiagram SVG V2 Double Any
 --renderNodeTree nodeTree = renderTree (\_ -> circle 1 # fc white `atop` text "FOO") arrowBetween (symmLayout nodeTree)
 renderNodeTree nodeTree = renderTree 
-    (\_ -> text "FOO" # font "monospace" # fontSize (local 0.5) `atop` square 1 # fc white) 
+    (\a -> letters2 a `atop` square 1 # fc white) 
     (~~) 
-    (symmLayout' (with{ _slHSep = 3,  _slVSep = 3}) nodeTree)
+    (symmLayout' (with{ _slHSep = 3,  _slVSep = 2}) nodeTree)
+  where
+     letters a = text (T.unpack $ abbreviate a) # font "monospace" # fontSize (local 0.5) 
+     number a = text (show . _nodeId $ a) # font "monospace" # fontSize (local 0.5) 
+     letters2 a = position  [(p2 (0,0.25), letters a), (p2 (0,-0.25), number a)]  
